04049b10a4cc3339e5f4ea4178ae124c14fb33d1
I H Shaon <mdshaonimran@gmail.com>
2014-04-24 20:48:30 +0600

    Create google-spreadsheet-app-script.js

---
+++ google-spreadsheet-app-script.js
@@ -0,0 +1,20 @@
+// check if the cell value contains certain sctring and color cell accordingly
+
+function onEdit() {
+  var sheet = SpreadsheetApp.getActiveSheet();
+  var rows = sheet.getDataRange();
+  var numRows = rows.getNumRows();
+  var values = rows.getValues();
+  
+  var cellvalue = new String( sheet.getActiveCell().getValue() );
+  Logger.log( cellvalue );
+  if ( cellvalue.match( 'failed' ) ) {
+    sheet.getActiveCell().setFontColor('red');
+  }
+  else if ( cellvalue.match( 'passed' ) ) {
+    sheet.getActiveCell().setFontColor('green');
+  }
+  else {
+    sheet.getActiveCell().setFontColor('black');
+  }
+};
