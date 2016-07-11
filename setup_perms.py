bdb2e437a664eeec0c9cdfd06e279a3b14b41bde
I H Shaon <mdshaonimran@gmail.com>
2016-07-11 15:56:19 -0700

    Update setup_perms.py

--- setup_perms.py
+++ setup_perms.py
@@ -13,31 +13,24 @@ class SetupPermission(object):
                          "var/log/eucalyptus",
                          "var/run/eucalyptus"]
 
-        self.eucaWraps = ["usr/lib/eucalyptus/euca_mountwrap",
-                          "usr/lib/eucalyptus/euca_rootwrap"]
+        self.eucaWraps = ["usr/lib/eucalyptus/euca_rootwrap"]
 
-        self.copyFiles = {"clc/modules/block-storage-common/udev/55-openiscsi.rules": "/etc/udev/rules.d/55-openiscsi.rules",
-                          "clc/modules/block-storage-common/udev/iscsidev.sh": "/etc/udev/scripts/iscsidev.sh",
-                          "clc/modules/block-storage/udev/rules.d/12-dm-permissions.rules": "/etc/udev/rules.d/12-dm-permissions.rules",
-                          "tools/eucalyptus-nc-libvirt.pkla": "/var/lib/polkit-1/localauthority/10-vendor.d/"}
-
-        self.eucaServices = ["usr/lib/systemd/system/eucalyptus-cc.service",
-                             "usr/lib/systemd/system/eucalyptus-cloud.service",
-                             "usr/lib/systemd/system/eucalyptus-cloud-upgrade.service",
-                             "usr/lib/systemd/system/eucalyptus-cluster.service",
-                             "usr/lib/systemd/system/eucalyptus-nc.service",
-                             "usr/lib/systemd/system/eucalyptus-node-keygen.service",
-                             "usr/lib/systemd/system/eucalyptus-node.service",
-                             "usr/lib/systemd/system/eucanetd.service"]
-
-        self.eucalyptusBinLn = ["usr/sbin/eucalyptus-cloud",
-                                "usr/sbin/eucalyptus-cluster",
-                                "usr/sbin/eucalyptus-node"]
+        self.copyFiles = {
+            "clc/modules/block-storage-common/udev/55-openiscsi.rules":
+                "/etc/udev/rules.d/55-openiscsi.rules",
+            "clc/modules/block-storage-common/udev/iscsidev.sh":
+                "/etc/udev/scripts/iscsidev.sh",
+            "clc/modules/block-storage/udev/rules.d/12-dm-permissions.rules":
+                "/etc/udev/rules.d/12-dm-permissions.rules",
+            "tools/eucalyptus-nc-libvirt.pkla":
+                "/var/lib/polkit-1/localauthority/10-vendor.d/"
+        }
 
         self.euca_home = None
         self.euca_source = None
 
-    def _walk_recursive(self, paths, fn, *params):
+    @staticmethod
+    def _walk_recursive(paths, fn, *params):
         """
         References:
             https://github.com/eucalyptus/eucalyptus/blob/maint-4.1/clc/eucadmin/eucadmin/utils.py
@@ -84,35 +77,29 @@ class SetupPermission(object):
     def main(self):
         parser = argparse.ArgumentParser()
         parser.add_argument("-e", "--euca-home", default="/")
-        parser.add_argument("-t", "--euca-source", default="tools/")
+        parser.add_argument("-s", "--euca-source", default=os.getcwd())
         args = parser.parse_args()
 
         if args.euca_source:
             self.euca_source = args.euca_source
 
-        # if os.environ.get('EUCALYPTUS'):
-        #     self.euca_home = os.environ.get('EUCALYPTUS')
-        # else:
-        #     self.euca_home = args.euca_home
+        if args.euca_home:
+            self.euca_home = args.euca_home
 
         if not os.path.exists("/etc/udev/scripts"):
             os.mkdir("/etc/udev/scripts")
 
-        # Copy Policy Kit file for NC
+        # Copy necessary files
         self.copy_files(self.copyFiles)
 
-        # copy eucalyptus (systemd) services
-        if self.euca_home is not "/":
-            self.copy_files(self.eucaServices, "/usr/lib/systemd/system/")
+        print self.euca_home
+        print self.euca_source
+        directories = map(lambda x: os.path.join(self.euca_home, x),
+                          self.eucaDirs)
 
-            # for eucaln in self.eucalyptusBinLn:
-            #     if os.path.join("/", eucaln):
-            #         os.remove(os.path.join("/", eucaln))
-            #     os.symlink(os.path.join(self.euca_home, eucaln), os.path.join("/", eucaln))
+        wrap_dirs = map(lambda x: os.path.join(self.euca_home, x),
+                        self.eucaWraps)
 
-        directories = map(lambda x: os.path.join(self.euca_home, x), self.eucaDirs)
-
-        wrap_dirs = map(lambda x: os.path.join(self.euca_home, x), self.eucaWraps)
         eucalyptus_uid = pwd.getpwnam('eucalyptus').pw_uid
         eucalyptus_gid = pwd.getpwnam('eucalyptus').pw_gid
         root_uid = pwd.getpwnam('root').pw_gid
@@ -124,6 +111,7 @@ class SetupPermission(object):
         for ew in wrap_dirs:
             os.chown(ew, root_uid, eucalyptus_gid)
             os.chmod(ew, 04750)
+
 
 if __name__ == "__main__":
     setup_perms = SetupPermission()
